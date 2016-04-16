ac706c1b93cc3a6afef7ccd3e27ef63db1839bf8
leptone <leptone55@gmail.com>
2016-04-16 00:00:37 -0700

    updated skills schema

--- skills.js
+++ skills.js
@@ -1,10 +1,35 @@
 module.exports = function() {
-	return [
-		{ name: 'Javascript', score: '1'},
-		{ name: 'Node.js', score: '2'},
-		{ name: 'HTML5', score: '3'},
-		{ name: 'CSS3', score: '4'},
-		{ name: 'SCSS', score: '5'},
-		{ name: 'SCSS', score: '5'},
-	]
+	return {
+		languages: [
+			{ name: 'Javascript', score: '4'},
+			{ name: 'Node.js', score: '4'},
+			{ name: 'MongDB', score: '4'},
+			{ name: 'HTML5', score: '5'},
+			{ name: 'CSS3', score: '5'},
+			{ name: 'SCSS', score: '4'},
+			{ name: 'Python', score: '2'},
+			{ name: 'Java', score: '2'},
+		],
+		libraries: [
+			{ name: 'Express.js', score: '4'},
+			{ name: 'Bootstrap', score: '4'},
+			{ name: 'Angular.js', score: '3'},
+			{ name: 'jQuery', score: '3'},
+		],
+		concepts: [
+			
+			{ name: 'Object Oriented Programming', score: '4'},
+			{ name: 'Functional Programming', score: '4'},
+			{ name: 'AJAX', score: '4'},
+			{ name: 'REST', score: '4'},
+			{ name: 'MVC', score: '4'},
+			{ name: 'NoSQL', score: '4'},
+		],
+		misc: [
+			{ name: 'Bash', score: '4'},
+			{ name: 'Git', score: '4'},
+			{ name: 'Browserify', score: '3'},
+			
+		]
+	}
 }()
